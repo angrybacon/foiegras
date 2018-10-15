f76612fed929873e29d01d2c1327ca62e3ab660d
Mathieu Marques <mathieumarques78@gmail.com>
2018-10-15 15:10:28 +0200

    Initial commit

---
+++ src/components/Application/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+
+class Application extends React.Component {
+  render() {
+    return <div class="application">Lorem</div>;
+  }
+}
+
+
+export default Application;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './components/Application';
+
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(<Application />, root);
+}
